7c4d9f54f1393b9d934d3ff2cb83b604a1744cd0
vijithra <vijithra120@gmail.com>
2024-09-25 16:50:44 +0530

    Initial Commit

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <aside
+    :class="['side-bar', { 'side-bar-expanded': isExpanded }]"
+    class="bg-black text-white overflow-hidden min-h-[100vh] p-[1rem]"
+  >
+    <div class="logo mb-[1rem]">
+      <img src="../assets/logo.png" alt="VueLogo" class="w-[2rem]" />
+    </div>
+    <div class="menu-toggle-wrap flex justify-end">
+      <button class="menu-toggle" @click="toggleMenu">
+        <span class="material-symbols-outlined">
+          {{
+            isExpanded
+              ? "keyboard_double_arrow_left"
+              : "keyboard_double_arrow_right"
+          }}
+        </span>
+      </button>
+    </div>
+    <div class="menu">
+      <div class="flex flex-col gap-y-8">
+        <router-link
+          to=""
+          class="text-white hover:bg-white hover:text-black rounded transition duration-300 flex px-2 py-2 items-center gap-x-2"
+        >
+          <span class="material-symbols-outlined"> home </span
+          ><span v-if="isExpanded">Home</span>
+        </router-link>
+        <router-link
+          to=""
+          class="text-white hover:bg-white hover:text-black rounded transition duration-300 flex px-2 py-2 items-center gap-x-2"
+        >
+          <span class="material-symbols-outlined"> person_add </span>
+          <span v-if="isExpanded">Add User</span>
+        </router-link>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    isExpanded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    toggleMenu() {
+      this.$emit("toggle-menu");
+    },
+  },
+};
+</script>
+
+<style>
+aside {
+  display: flex;
+  flex-direction: column;
+  width: calc(2rem + 32px);
+  transition: width 0.2s ease-out;
+}
+
+.side-bar-expanded {
+  width: 250px;
+}
+
+@media (max-width: 768px) {
+  aside {
+    position: fixed;
+    z-index: 99;
+    height: 100vh;
+  }
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home flex bg-gray-100 min-h-screen">
+    <!-- Sidebar with toggle -->
+    <SideBar :isExpanded="isSidebarExpanded" @toggle-menu="toggleSidebar" />
+
+    <!-- Main content adjusts based on sidebar state -->
+    <div
+      :class="[
+        'w-full p-4 transition-all duration-300',
+        { 'ml-[10px]': isSidebarExpanded },
+      ]"
+    >
+      <p>This is Home Page</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/SideBar.vue";
+
+export default {
+  data() {
+    return {
+      isSidebarExpanded: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarExpanded = !this.isSidebarExpanded;
+    },
+  },
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<style scoped>
+/* Additional styles, if necessary */
+</style>
