e89843f4177fa9e050eca289d2dc9aded072e970
vijithra <vijithra120@gmail.com>
2024-09-26 15:12:03 +0530

    Created Add Staff Form

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AddStudent from "@/views/Student/AddStudent.vue";
 import StudentList from "@/views/Student/StudentList.vue";
+import AddStaff from "@/views/Staff/AddStaff.vue";
 
 const routes = [
   {
@@ -27,6 +28,11 @@ const routes = [
     path: "/studentlist",
     name: "studentlist",
     component: StudentList,
+  },
+  {
+    path: "/addstaff",
+    name: "addstaff",
+    component: AddStaff,
   },
 ];
 
